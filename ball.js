a94c0ce54a9dbba97cbd9fd21b2b8b1d19e71240
alwin8907 <73284125+alwin8907@users.noreply.github.com>
2020-12-18 15:46:25 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,16 @@
+class ball {
+    constructor(x,y,width,height){
+var options={density:1}
+this.body=Bodies.rectangle(x,y,width,height,options)
+this.width=width
+this.height=height
+World.add(world,this.body) 
+    }
+    display(){
+        push()
+        translate(this.body.position.x,this.body.position.y)
+        rotate(this.body.angle)
+        ellipse(0,0,this.width,this.height)
+        pop()
+    }
+}
